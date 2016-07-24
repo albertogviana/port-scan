215912f8dc1364b0628bf1a2e19a3531e82c7dbe
Alberto Viana <albertogviana@gmail.com>
2016-07-24 21:47:40 +0200

    Refactoring

--- main.go
+++ main.go
@@ -1,29 +1,6 @@
 package main
 
-import (
-	"./nmap"
-	"fmt"
-)
 
 func main() {
-	host := ""
-	result := nmap.Run(host)
-
-	fmt.Print(result)
-	//
-	//openPorts := grep(output)
-	//
-	//v := convertStringToInt(openPorts)
-	//
-	//openPortsConfiguration := []int{
-	//	22,
-	//	443,
-	//	9999,
-	//}
-	//
-	//expectedUnfound, unexpectedFound := analyseResults(openPortsConfiguration, v)
-	//
-	//message := message(expectedUnfound, unexpectedFound)
-	//fmt.Print(message)
 
 }
